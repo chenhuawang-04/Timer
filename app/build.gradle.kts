efa9d0dfa3e3419d69dff42a33ecd48fffd5ceab
chenhuawang-04 <inonru@yandex.com>
2026-06-09 17:41:56 +0800

    chore: bump version to 1.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,8 +40,8 @@ android {
         applicationId = "com.timer.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.0.0"
+        versionCode = 3
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
